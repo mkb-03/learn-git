316bf792b7473cc2a8f748e2fcdd0130d908dca5
mkb <anmolkb1020@gmail.com>
2023-04-25 21:20:40 +0500

    added another file

---
+++ difference.js
@@ -0,0 +1,4 @@
+let a = 9 , b = 8;
+let diff = a-b
+
+console.log("The difference of two numbers is: ", diff );
---
+++ multiply.js
@@ -0,0 +1,4 @@
+let a = 9 , b = 8;
+let product = a*b
+
+console.log("The product of two numbers is: ", product );
